9c277152c5213d838827c192f144b4173cc69552
Styrse <styrbjoern@me.com>
2024-12-06 15:31:19 +0100

    Styr - Clothes2

--- src/main/java/ice/ice/Clothes.java
+++ src/main/java/ice/ice/Clothes.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Clothes {
     private String type;
     private int weightGrams;
-    private double co2Gram;
+    private ArrayList<Fabric> fabrics;
     private ArrayList<Clothes> clothes;
 
     public Clothes(String type, int weight) {
@@ -13,7 +13,28 @@ public class Clothes {
         this.weightGrams = weight;
     }
 
+    public void setupClothes(){
+        loadFabrics();
+        loadClothes();
+    }
+
     public double calCo2PrItem(Fabric fabricType){
         return fabricType.getCo2Gram() * weightGrams;
+    }
+
+    public void loadFabrics(){
+        ArrayList<String> temp = FileIO.fileReader("data/emission/fabric.csv");
+        for (String data : temp){
+            String[] tempString = data.split(";");
+            fabrics.add(new Fabric(tempString[0], Double.parseDouble(tempString[1])));
+        }
+    }
+
+    public void loadClothes(){
+        ArrayList<String> temp = FileIO.fileReader("data/emission/clothes.csv");
+        for (String data : temp){
+            String[] tempString = data.split(";");
+            clothes.add(new Clothes(tempString[0], Integer.parseInt(tempString[1])));
+        }
     }
 }
