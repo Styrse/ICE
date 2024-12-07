ddca9bb76f5e92590ad54cd523d6845ee5030d00
DDVoss <danieldvoss@gmail.com>
2024-12-07 12:26:45 +0100

    added transport
    Method for calculating emission pr minute, including two void methods that is gonna load the vehicle data for public and car data.

--- src/main/java/ice/ice/Transport.java
+++ src/main/java/ice/ice/Transport.java
@@ -1,15 +1,31 @@
 package ice.ice;
 
-public abstract class Transport extends Emission  {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public abstract class Transport  {
     private String VehicleType;
+    private double averageSpeed;
     private double co2PrKm;
 
-    public Transport(String vehicleType, double co2PrKm, double co2Gram) {
-        super(co2Gram);
+
+    public Transport(String vehicleType, double averageSpeed, double co2PrKm) {
         this.VehicleType = vehicleType;
+        this.averageSpeed = averageSpeed;
         this.co2PrKm = co2PrKm;
     }
-    public void convertTimeToKm()   {
-        // (hastighed:86,6/tid:3060)*(køretid*60)
+
+    public void loadPublicTransportData()  {
+
+    }
+
+    public void loadCarData()   {
+
+    }
+
+    public double emissionPrMin()   {
+        double kmPrMin = averageSpeed/60;
+        double result = kmPrMin * co2PrKm;
+        return result;
     }
 }
