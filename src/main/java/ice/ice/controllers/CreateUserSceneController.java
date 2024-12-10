c55f50f7517e00715f8f273e30fee6cc3398cb23
Styrse <styrbjoern@me.com>
2024-12-11 00:48:38 +0100

    Styr - Create user

--- src/main/java/ice/ice/controllers/CreateUserSceneController.java
+++ src/main/java/ice/ice/controllers/CreateUserSceneController.java
@@ -1,47 +1,58 @@
 package ice.ice.controllers;
 
+import ice.ice.UserMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CreateUserSceneController {
+    public void initialize(){
+        genderChoiceBox.getItems().addAll("Male", "Female", "Non-binary", "Transgender", "Other", "Prefer not to say");
+        genderChoiceBox.setValue("Choose gender");
 
-    @FXML
-    private ChoiceBox<?> countryChoiceBox;
-
-    @FXML
-    private ChoiceBox<?> genderChoiceBox;
-
-    @FXML
-    void handleAddress(ActionEvent event) {
-
+        countryChoiceBox.getItems().addAll("Denmark", "Sweden", "Italy");
+        countryChoiceBox.setValue("Choose country");
     }
 
     @FXML
-    void handleBirthday(ActionEvent event) {
+    private TextField addressTextField;
 
-    }
+    @FXML
+    private DatePicker birthdayPicker;
+
+    @FXML
+    private ChoiceBox<String> countryChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> genderChoiceBox;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    @FXML
+    private TextField usernameTextField;
 
     @FXML
     void handleCreate(ActionEvent event) {
+        String username = usernameTextField.getText();
+        String password = passwordPasswordField.getText();
+        String name = nameTextField.getText();
+        String gender = String.valueOf(genderChoiceBox.getSelectionModel().getSelectedItem());
+        String birthday = birthdayPicker.getId();
+        String address = addressTextField.getText();
+        String country = String.valueOf(countryChoiceBox.getSelectionModel().getSelectedItem());
 
-    }
+        if (UserMapper.checkDuplicateUsername(username)){
 
-    @FXML
-    void handleName(ActionEvent event) {
-
-    }
-
-    @FXML
-    void handlePassword(ActionEvent event) {
-
-    }
-
-    @FXML
-    void handleUsername(ActionEvent event) {
-
+        }
     }
 
     @FXML
