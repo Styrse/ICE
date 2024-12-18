bcd4dfd0adde812a5acafe94f21129fe24a3d65c
Styrse <styrbjoern@me.com>
2024-12-18 05:22:13 +0100

    Styr - Fix

--- src/main/java/ice/ice/controllers/MainMenuSceneController.java
+++ src/main/java/ice/ice/controllers/MainMenuSceneController.java
@@ -5,7 +5,6 @@ import ice.ice.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
