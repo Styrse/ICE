efb2a43aa2a01171a0c659d59a4e1573a3db1059
Styrse <styrbjoern@me.com>
2024-12-18 02:15:42 +0100

    Styr - Plant trees

--- src/main/java/ice/ice/controllers/PlantSceneController.java
+++ src/main/java/ice/ice/controllers/PlantSceneController.java
@@ -1,4 +1,42 @@
 package ice.ice.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class PlantSceneController {
+
+    @FXML
+    private Label invalidInput;
+
+    @FXML
+    private TextField selfPlantedTreesTextField;
+
+    @FXML
+    private TextField payToPlantTreesTextField;
+
+    public TextField getPayToPlantTreeTextField() {
+        return payToPlantTreesTextField;
+    }
+
+    public TextField getSelfPlantedTreesTextField() {
+        return selfPlantedTreesTextField;
+    }
+
+    @FXML
+    void handlePlantButton(ActionEvent event) {
+        String selfPlantTrees = selfPlantedTreesTextField.getText().trim();
+        String payToPlantTrees = payToPlantTreesTextField.getText().trim();
+
+        if (!selfPlantTrees.isEmpty() && !payToPlantTrees.isEmpty()){
+
+        }
+    }
+
+    @FXML
+    void handleTeamTreesHyperlink(ActionEvent event) {
+
+    }
+
 }
